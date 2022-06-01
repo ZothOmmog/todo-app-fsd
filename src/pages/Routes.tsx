d857efb725cc393d08612db12b798ad4463e408b
ZothOmmog <abpstd@gmail.com>
2022-06-01 21:44:46 +0300

    Добавил страницу с детальной информацией по таску

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate, Route, Routes as RoutesBase } from 'react-router-dom';
 
+import { TaskDetailsPage } from './taskDetailsPage';
 import { TaskListPage } from './taskListPage';
 
 export const Routes: React.FC = () => {
   return (
     <RoutesBase>
       <Route index element={<TaskListPage />} />
+      <Route path=":taskId" element={<TaskDetailsPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </RoutesBase>
   );
---
+++ src/pages/taskDetailsPage/TaskDetailsPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TaskDetailsPage: React.FC = () => {
+  return <div>TaskDetailsPage</div>;
+};

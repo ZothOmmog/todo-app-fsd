c8958d35c5efd8fe9d0fc36c21988fe3f6de1e80
ZothOmmog <abpstd@gmail.com>
2022-06-03 00:26:12 +0300

    Замокал время создания

--- src/entities/task/ui/taskRow/TaskRow.tsx
+++ src/entities/task/ui/taskRow/TaskRow.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 
 import { $taskShortMap } from '../../model';
 
+import style from './taskRow.module.scss';
+
 type TaskRowProps = { id: string; Actions: React.FC<{ id: string }> };
 
 const TaskRow: React.FC<TaskRowProps> = (props) => {
@@ -19,12 +21,14 @@ const TaskRow: React.FC<TaskRowProps> = (props) => {
     <div>
       <Card>
         <Card.Meta
+          className={style.meta}
           title={
             <Space size="small">
               <Actions id={id} />
               {title}
             </Space>
           }
+          description="20.12.2021 12:35"
         />
       </Card>
     </div>
